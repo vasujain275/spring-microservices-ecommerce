5849d09bce943aa72d0a52190b949edb04bb5530
vasujain275 <vasujain275@gmail.com>
2025-02-27 00:14:39 +0530

    feat: added integration test for products

--- product-service/src/test/java/me/vasujain/microservices/product_service/ProductServiceApplicationTests.java
+++ product-service/src/test/java/me/vasujain/microservices/product_service/ProductServiceApplicationTests.java
@@ -1,15 +1,55 @@
 package me.vasujain.microservices.product_service;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+	@ServiceConnection
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo");
+
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mongoDBContainer.start();
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() {
+		String requestBody = """
+				{
+				  "name":"Iphone 15",
+				  "description":"New gen Iphone",
+				  "price":14500
+				}
+				""";
+
+		RestAssured.given()
+				.contentType("application/json")
+				.body(requestBody)
+				.when()
+				.post("/api/v1/products")
+				.then()
+				.statusCode(201)
+		;
+
+
+
 	}
 
 }
