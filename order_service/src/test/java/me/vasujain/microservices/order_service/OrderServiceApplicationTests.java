34ff081510a6dc738a3ddb3d7833c750b656ac5d
vasujain275 <vasujain275@gmail.com>
2025-02-27 10:06:03 +0530

    feat: basic testing setup for order service

--- order_service/src/test/java/me/vasujain/microservices/order_service/OrderServiceApplicationTests.java
+++ order_service/src/test/java/me/vasujain/microservices/order_service/OrderServiceApplicationTests.java
@@ -1,12 +1,26 @@
 package me.vasujain.microservices.order_service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderServiceApplicationTests {
+
+	@ServiceConnection
+	static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer(PostgreSQLContainer.IMAGE);
+
+	@LocalServerPort
+	private Integer port;
+
+	@BeforeEach
+	void setup(){
+	}
 
 	@Test
 	void contextLoads() {
